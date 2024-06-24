7d24b6af5c47f41384e3f6867ae9d86d1e640dd9
Wolfremium13 <wolfremiuminformatica@gmail.com>
2024-06-24 09:07:53 +0100

    ♻ format

--- kata-word-wrap/src/Lines.cs
+++ kata-word-wrap/src/Lines.cs
@@ -3,14 +3,6 @@ namespace kata_word_wrap;
 public class Lines
 {
     private readonly List<Line> _lines = [];
-
-    public void Append(Line line)
-    {
-        _lines.Add(line);
-    }
-
-    public override string ToString()
-    {
-        return string.Join("\n", _lines.Select(line => line.ToString()));
-    }
+    public void Append(Line line) => _lines.Add(line);
+    public override string ToString() => string.Join("\n", _lines.Select(line => line.ToString()));
 }
